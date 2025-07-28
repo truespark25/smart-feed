fbdc44550ba53ce686826eeb027465287716d0ad
truespark25 <info.truespark@gmail.com>
2025-07-28 19:07:35 +0530

    Update subscribe.ts

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,22 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email } = req.body;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
-
-  const { email } = req.body;
-
-  try {
-    await fetch('https://api.buttondown.email/v1/subscribers', {
+    const response = await fetch('https://api.buttondown.email/v1/subscribers', {
       method: 'POST',
       headers: {
-        Authorization: `Token 0bdd9044-bc84-4e33-bd9a-ce4432988750`,
+        Authorization: `Token ${process.env.0bdd9044-bc84-4e33-bd9a-ce4432988750}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email }),
     });
 
-    res.status(200).json({ message: 'Subscribed' });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to subscribe' });
+    if (response.ok) {
+      res.status(200).json({ message: 'Subscribed!' });
+    } else {
+      res.status(500).json({ message: 'Failed to subscribe' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
